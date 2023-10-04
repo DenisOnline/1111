83e0fec6617b262807ae9106971a70ca7efeb2a9
DenisOnline <denis@shilzhenko-it.ru>
2023-10-04 18:08:54 +0300

    lesson-11

---
+++ lesson-11.py
@@ -0,0 +1,22 @@
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, filters
+from telegram import Update
+
+
+def start(update=Update, context=CallbackContext):
+    update.message.reply_text("Hi User")
+
+
+def echo(update=Update, context=CallbackContext):
+    update.message.reply_text(update.message.text)
+
+
+def main():
+    updater = Updater("6368025272:AAFV-BdDWBN9GylATXgViVVRPiys0uWKz_Y")
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo()))
+    updater.start_polling()
+    updater.idle()
+
+
+main()
